34f40463f7060683d021088bc6e67b7b63194c8a
UjjawalGusain <ujjawalgusain31@gmail.com>
2025-10-22 20:40:56 +0530

    Testing audio

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -185,6 +185,14 @@ const Chat = ({ selectedChat }) => {
                 >
                     Send
                 </button>
+                
+                <button
+                    onClick={() => notificationSound.current.play()}
+                    className="bg-yellow-400 p-2 rounded"
+                >
+                    Test Sound
+                </button>
+                
             </form>
         </div>
     );
